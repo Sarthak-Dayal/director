33e458eb5af61dda03adef714248209c4d7793eb
Sarthak Dayal <dayalsarthak5@gmail.com>
2025-05-20 23:11:50 -0500

    Corrections to ACROModule and changes to configs

--- embodied/agents/director/ACROModule.py
+++ embodied/agents/director/ACROModule.py
@@ -28,9 +28,10 @@ class ACROModule(tfutils.Module):
 
     # ACRO Model
     # Translation from World Model to ACRO space
-    # ACRO to skill VAE
+    # SAR TODO ACRO to skill VAE
 
     def __init__(self, act_space, config):
+        super().__init__()
         self.config = config
 
         # ACRO Model
@@ -39,10 +40,10 @@ class ACROModule(tfutils.Module):
         self.acro_encoder_backbone = nets.MultiEncoder({"frame_stack": self.acro_state_size}, **self.config.acro_encoder_backbone)
         self.acro_embedding_head = nets.Dense(units=self.config.acro_embed_size, **self.config.acro_embedding_head)
 
-        self.acro_action_backbone = nets.MLP(act_space.shape, **self.config.acro_action_backbone)
+        self.acro_action_head = nets.MLP(act_space.shape, **self.config.acro_action_head)
 
         # Translation Layer
-        self.wm_to_acro_backbone = nets.MLP(shape=(self.config.acro_embed_size,), **self.config.wm_to_acro_transition)
+        self.wm_to_acro_backbone = nets.MLP(shape=(self.config.acro_embed_size,), **self.config.wm_to_acro_backbone)
 
     def embed_acro(self, frame_stack):
         hidden = self.acro_encoder_backbone({"frame_stack": frame_stack})
@@ -52,28 +53,28 @@ class ACROModule(tfutils.Module):
         return self.wm_to_acro_backbone(wm_state)
 
     def train(self, data):
-        # data is time-major ([T, batch, ...])
+        # data is allegedly time-major ([T, batch, ...])
         images = data['image']
         actions = data['action']
-        wm_states = data['wm_state'] # TODO input this in training data
+        wm_states = data['wm_state'] # SAR TODO input this in training data
 
         # dimensions
         T = tf.shape(images)[0]
         batch = tf.shape(images)[1]
-        k = self.config.acro.k_step
-        K = self.config.frame_stack
+        k = self.config.acro_k_step
+        frame_k = self.config.frame_stack
 
-        fs_cur = FrameStack(K) # current frame stack
-        fs_fut = FrameStack(K) # future frame stack, for k-step prediction loss
+        fs_cur = FrameStack(frame_k) # current frame stack
+        fs_fut = FrameStack(frame_k) # future frame stack, for k-step prediction loss
         metrics = {}
 
         # prefill frame stacks
-        for t in range(K):
+        for t in range(frame_k):
             fs_cur.add_frame(images[t])
             fs_fut.add_frame(images[t + k])
 
         # slide along time
-        for i in range(int(K - 1), int(T - k)):
+        for i in range(int(frame_k - 1), int(T - k)):
             acro_state_cur = self.embed_acro(fs_cur.get_frames())
             acro_state_fut = self.embed_acro(fs_fut.get_frames())
 
@@ -86,8 +87,7 @@ class ACROModule(tfutils.Module):
                 wm_loss = tf.reduce_mean(tf.square(pred_wm - acro_state_fut)) # MSE Loss
 
             with tf.GradientTape() as action_tape:
-                combined = tf.concat([acro_state_cur, acro_state_fut], axis=-1) # K-step apart states
-                pred_action = self.acro_action_backbone(combined)
+                pred_action = self.acro_action_head({'state_t': acro_state_cur, 'state_tk': acro_state_fut})
                 norm_true = tf.nn.l2_normalize(true_action, axis=-1)
                 action_loss = tf.reduce_mean(tf.square(pred_action - norm_true))
 
